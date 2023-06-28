b4894713a5fa94aa95bf0ac5487a9dbb8374fb3d
Volcomix <Volcomix@users.noreply.github.com>
2023-06-28 13:05:06 +0200

    Fix MUI styles not loaded in PiP window

--- src/components/PiPWindow/PiPWindow.tsx
+++ src/components/PiPWindow/PiPWindow.tsx
@@ -1,3 +1,5 @@
+import createCache from '@emotion/cache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
 import Button from '@mui/material/Button';
 import { useRef } from 'react';
 import { createPortal } from 'react-dom';
@@ -22,6 +24,7 @@ const PiPWindow = () => {
   const { pipWindow } = usePictureInPicture();
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const cssCacheRef = useRef<EmotionCache>();
 
   const videoElement = videoRef.current;
   if (cameraStream && videoElement) {
@@ -30,39 +33,48 @@ const PiPWindow = () => {
 
   if (!pipWindow) return null;
 
+  if (!cssCacheRef.current) {
+    cssCacheRef.current = createCache({
+      key: 'external',
+      container: pipWindow.document.body,
+    });
+  }
+
   return createPortal(
-    <div className={styles.root}>
-      <video
-        className={styles.camera}
-        autoPlay
-        playsInline
-        muted
-        controls={false}
-        ref={videoRef}
-      />
-      <div className={styles.toolbar}>
-        <Button size="small" onClick={startRecording} disabled={isRecording}>
-          Start
-        </Button>
-        <Button size="small" onClick={stopRecording} disabled={!isRecording}>
-          Stop
-        </Button>
-        <Button
-          size="small"
-          onClick={pauseRecording}
-          disabled={!isRecording || isPaused}
-        >
-          Pause
-        </Button>
-        <Button
-          size="small"
-          onClick={resumeRecording}
-          disabled={!isRecording || !isPaused}
-        >
-          Resume
-        </Button>
+    <CacheProvider value={cssCacheRef.current}>
+      <div className={styles.root}>
+        <video
+          className={styles.camera}
+          autoPlay
+          playsInline
+          muted
+          controls={false}
+          ref={videoRef}
+        />
+        <div className={styles.toolbar}>
+          <Button size="small" onClick={startRecording} disabled={isRecording}>
+            Start
+          </Button>
+          <Button size="small" onClick={stopRecording} disabled={!isRecording}>
+            Stop
+          </Button>
+          <Button
+            size="small"
+            onClick={pauseRecording}
+            disabled={!isRecording || isPaused}
+          >
+            Pause
+          </Button>
+          <Button
+            size="small"
+            onClick={resumeRecording}
+            disabled={!isRecording || !isPaused}
+          >
+            Resume
+          </Button>
+        </div>
       </div>
-    </div>,
+    </CacheProvider>,
     pipWindow.document.body
   );
 };
