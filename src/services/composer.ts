5e0acd67c53de086cdd27326f5b37a73eef41d4c
Volcomix <Volcomix@users.noreply.github.com>
2023-07-05 10:52:23 +0200

    Experiment with requestAnimationFrame

--- src/services/composer.ts
+++ src/services/composer.ts
@@ -9,91 +9,71 @@ export const composeStreams = (
   microphoneStream: MediaStream | null,
   screenshareStream: MediaStream
 ): MediaStream => {
-  const cameraTrack = cameraStream?.getVideoTracks()[0];
   const microphoneTrack = microphoneStream?.getAudioTracks()[0];
-  const screenshareTrack = screenshareStream.getVideoTracks()[0];
 
-  const screenshareProcessor = new MediaStreamTrackProcessor({
-    track: screenshareTrack,
-  });
-  const recordingGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
+  const screenshareVideo = document.createElement('video');
+  screenshareVideo.style.position = 'fixed';
+  screenshareVideo.style.visibility = 'hidden';
+  screenshareVideo.autoplay = true;
+  screenshareVideo.muted = true;
+  document.body.appendChild(screenshareVideo);
 
-  if (cameraTrack) {
-    const cameraProcessor = new MediaStreamTrackProcessor({
-      track: cameraTrack,
-    });
-    const screenshareReader = screenshareProcessor.readable.getReader();
+  screenshareVideo.srcObject = screenshareStream;
 
-    const canvas = new OffscreenCanvas(0, 0);
-    const ctx = canvas.getContext('2d');
+  const cameraVideo = document.createElement('video');
+  cameraVideo.style.position = 'fixed';
+  cameraVideo.style.visibility = 'hidden';
+  cameraVideo.autoplay = true;
+  cameraVideo.muted = true;
+  document.body.appendChild(cameraVideo);
 
-    if (!ctx) {
-      throw new Error('Canvas API not supported');
-    }
+  cameraVideo.srcObject = cameraStream;
 
-    let latestScreenshareFrame: VideoFrame | undefined;
+  const canvas = document.createElement('canvas');
+  canvas.width = screenshareVideo.videoWidth || 1920;
+  canvas.height = screenshareVideo.videoHeight || 1080;
+  const ctx = canvas.getContext('2d');
 
-    const transformer = new TransformStream({
-      async transform(cameraFrame: VideoFrame, controller) {
-        if (latestScreenshareFrame) {
-          // Awaiting the read operation would block the recording until
-          // the next frame, which could come way later when the screenshare
-          // is fully static
-          screenshareReader.read().then(({ value: screenshareFrame }) => {
-            latestScreenshareFrame?.close();
-            latestScreenshareFrame = screenshareFrame;
-          });
-        } else {
-          // Waits for the 1st frame to initialize the canvas dimensions
-          const { value: screenshareFrame } = await screenshareReader.read();
-          latestScreenshareFrame = screenshareFrame;
-        }
-        if (latestScreenshareFrame) {
-          canvas.width = latestScreenshareFrame.displayWidth;
-          canvas.height = latestScreenshareFrame.displayHeight;
-          ctx.drawImage(latestScreenshareFrame, 0, 0);
-        }
-
-        ctx.beginPath();
-        ctx.roundRect(
-          canvas.width - CAMERA_WIDTH - CAMERA_MARGIN_RIGHT,
-          canvas.height - CAMERA_HEIGHT - CAMERA_MARGIN_BOTTOM,
-          CAMERA_WIDTH,
-          CAMERA_HEIGHT,
-          CAMERA_BORDER_RADIUS
-        );
-        ctx.clip();
-
-        ctx.drawImage(
-          cameraFrame,
-          (cameraFrame.displayWidth - cameraFrame.displayHeight) / 2,
-          0,
-          cameraFrame.displayHeight,
-          cameraFrame.displayHeight,
-          canvas.width - CAMERA_WIDTH - CAMERA_MARGIN_RIGHT,
-          canvas.height - CAMERA_HEIGHT - CAMERA_MARGIN_BOTTOM,
-          CAMERA_WIDTH,
-          CAMERA_HEIGHT
-        );
-
-        ctx.restore();
-
-        const newFrame = new VideoFrame(canvas, {
-          timestamp: cameraFrame.timestamp,
-        });
-        cameraFrame.close();
-        controller.enqueue(newFrame);
-      },
-    });
-
-    cameraProcessor.readable
-      .pipeThrough(transformer)
-      .pipeTo(recordingGenerator.writable);
-  } else {
-    screenshareProcessor.readable.pipeTo(recordingGenerator.writable);
+  if (!ctx) {
+    throw new Error('Canvas API not supported');
   }
 
-  const recordingStream = new MediaStream([recordingGenerator]);
+  const render = () => {
+    canvas.width = screenshareVideo.videoWidth || 1920;
+    canvas.height = screenshareVideo.videoHeight || 1080;
+
+    ctx.drawImage(screenshareVideo, 0, 0);
+
+    ctx.beginPath();
+    ctx.roundRect(
+      canvas.width - CAMERA_WIDTH - CAMERA_MARGIN_RIGHT,
+      canvas.height - CAMERA_HEIGHT - CAMERA_MARGIN_BOTTOM,
+      CAMERA_WIDTH,
+      CAMERA_HEIGHT,
+      CAMERA_BORDER_RADIUS
+    );
+    ctx.clip();
+
+    ctx.drawImage(
+      cameraVideo,
+      (cameraVideo.videoWidth - cameraVideo.videoHeight) / 2,
+      0,
+      cameraVideo.videoHeight,
+      cameraVideo.videoHeight,
+      canvas.width - CAMERA_WIDTH - CAMERA_MARGIN_RIGHT,
+      canvas.height - CAMERA_HEIGHT - CAMERA_MARGIN_BOTTOM,
+      CAMERA_WIDTH,
+      CAMERA_HEIGHT
+    );
+
+    ctx.restore();
+
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame(render);
+
+  const recordingStream = canvas.captureStream();
   if (microphoneTrack) {
     recordingStream.addTrack(microphoneTrack);
   }
