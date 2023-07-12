1d5d91747acde8524dccdd496b6a8659bab19684
Volcomix <Volcomix@users.noreply.github.com>
2023-07-12 18:15:28 +0200

    Bootstrap countdown animation

--- src/components/PiPRecordButton/PiPRecordButton.tsx
+++ src/components/PiPRecordButton/PiPRecordButton.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 
 import RecordButton, { RecordButtonProps } from 'components/RecordButton';
 
@@ -11,8 +11,17 @@ const PiPRecordButton = (
   { className, ...props }: PiPRecordButtonProps,
   ref: React.ForwardedRef<HTMLButtonElement>
 ) => {
+  const [countingDown] = useState(false);
+
   return (
-    <RecordButton className={cx(styles.root, className)} ref={ref} {...props} />
+    <RecordButton
+      className={cx(styles.root, className, {
+        [styles.countingDown]: countingDown,
+      })}
+      classes={{ icon: styles.icon }}
+      ref={ref}
+      {...props}
+    />
   );
 };
 
