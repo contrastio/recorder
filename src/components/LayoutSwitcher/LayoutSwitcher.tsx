45939b7bd11520d408da97a8c2f6c93b5b4007e2
Volcomix <Volcomix@users.noreply.github.com>
2023-07-17 19:02:24 +0200

    Add screen only layout button

--- src/components/LayoutSwitcher/LayoutSwitcher.tsx
+++ src/components/LayoutSwitcher/LayoutSwitcher.tsx
@@ -1,7 +1,19 @@
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+import ScreenIcon from './ScreenIcon';
+
 import styles from './LayoutSwitcher.module.css';
 
 const LayoutSwitcher = () => {
-  return <div className={styles.root}></div>;
+  return (
+    <ToggleButtonGroup className={styles.root}>
+      <ToggleButton value="screenOnly">
+        <ScreenIcon />
+        Screen only
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
 };
 
 export default LayoutSwitcher;
