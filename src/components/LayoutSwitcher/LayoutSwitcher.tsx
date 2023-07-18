7a5117f7f05e65a30c43562afe58d256790ca0f4
Sébastien Jalliffier Verne <Volcomix@users.noreply.github.com>
2023-07-18 09:48:49 +0200

    Add screen and camera layout button (#11)

--- src/components/LayoutSwitcher/LayoutSwitcher.tsx
+++ src/components/LayoutSwitcher/LayoutSwitcher.tsx
@@ -1,6 +1,7 @@
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
+import ScreenAndCameraIcon from './ScreenAndCameraIcon';
 import ScreenIcon from './ScreenIcon';
 
 import styles from './LayoutSwitcher.module.css';
@@ -11,6 +12,10 @@ const LayoutSwitcher = () => {
       <ToggleButton value="screenOnly">
         <ScreenIcon />
         Screen only
+      </ToggleButton>
+      <ToggleButton value="screenAndCamera">
+        <ScreenAndCameraIcon />
+        Screen and camera
       </ToggleButton>
     </ToggleButtonGroup>
   );
