4e42afec176ae678bab035318f7a981cf90dd284
Volcomix <Volcomix@users.noreply.github.com>
2023-07-18 14:55:34 +0200

    Update UI layout when switching layout

--- src/hooks/useVideoSource.ts
+++ src/hooks/useVideoSource.ts
@@ -1,11 +1,17 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 
 const useVideoSource = (videoStream: MediaStream | null) => {
+  const videoElementRef = useRef<HTMLVideoElement | null>(null);
+
   return useCallback(
     (videoElement: HTMLVideoElement | null) => {
       if (videoElement) {
         videoElement.srcObject = videoStream;
+      } else if (videoElementRef.current) {
+        videoElementRef.current.srcObject = null;
+        videoElementRef.current.load();
       }
+      videoElementRef.current = videoElement;
     },
     [videoStream],
   );
