724172f6709dcec2a5e0fd74e02ce8a2461af429
Sébastien Jalliffier Verne <Volcomix@users.noreply.github.com>
2023-07-18 18:58:21 +0200

    Stop recording when changing device selection (#21)

--- src/hooks/useCamera.ts
+++ src/hooks/useCamera.ts
@@ -1,5 +1,8 @@
 import { useCallback, useRef } from 'react';
 
+import { useLayout } from 'contexts/layout';
+import { usePictureInPicture } from 'contexts/pictureInPicture';
+import { useRecording } from 'contexts/recording';
 import { useStreams } from 'contexts/streams';
 import { getCameraStream } from 'services/mediaDevices';
 
@@ -7,6 +10,18 @@ const useCamera = (deviceId: string, enabled: boolean) => {
   if (!enabled) {
     deviceId = '';
   }
+
+  const { layout } = useLayout();
+  const layoutRef = useRef(layout);
+  layoutRef.current = layout;
+
+  const { isRecording } = useRecording();
+  const isRecordingRef = useRef(isRecording);
+  isRecordingRef.current = isRecording;
+
+  const { pipWindow } = usePictureInPicture();
+  const pipWindowRef = useRef(pipWindow);
+  pipWindowRef.current = pipWindow;
 
   const { cameraStream, setCameraStream } = useStreams();
 
@@ -23,6 +38,9 @@ const useCamera = (deviceId: string, enabled: boolean) => {
       }
       if (deviceId === deviceIdRef.current) {
         return;
+      }
+      if (isRecordingRef.current && layoutRef.current !== 'screenOnly') {
+        pipWindowRef.current?.close();
       }
       cameraStreamRef.current?.getTracks().forEach((track) => track.stop());
       setCameraStream(null);
