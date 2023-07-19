c411bf602f123b5a6e89f2904b135b939cbe5aef
Sébastien Jalliffier Verne <Volcomix@users.noreply.github.com>
2023-07-19 13:54:59 +0200

    Fix main record button alignment (#24)

--- src/components/GitHubButton/GitHubButton.tsx
+++ src/components/GitHubButton/GitHubButton.tsx
@@ -1,9 +1,12 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Button from '@mui/material/Button';
 
+import styles from './GitHubButton.module.css';
+
 const GitHubButton = () => {
   return (
     <Button
+      className={styles.root}
       href="https://github.com/contrastio/recorder"
       target="_blank"
       startIcon={<GitHubIcon />}
